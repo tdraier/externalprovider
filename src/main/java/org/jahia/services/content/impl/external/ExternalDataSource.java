87148616d4d06578f59f80fbcf247f98e2d9d731
Thomas Draier <tdraier@jahia.com>
2012-10-31 11:21:02 +0100

    initial commit

---
+++ src/main/java/org/jahia/services/content/impl/external/ExternalDataSource.java
@@ -0,0 +1,27 @@
+package org.jahia.services.content.impl.external;
+
+import javax.jcr.ItemNotFoundException;
+import javax.jcr.PathNotFoundException;
+import java.util.List;
+import java.util.Map;
+
+public interface ExternalDataSource {
+    boolean isSupportsUuid();
+
+    boolean isSupportsSearch();
+
+    List<String> getSupportedNodeTypes();
+
+    ExternalData getItemByIdentifier(String identifier) throws ItemNotFoundException;
+
+    ExternalData getItemByPath(String path) throws PathNotFoundException;
+
+    List<String> getChildren(String path);
+
+    void removeItemByPath(String path) throws PathNotFoundException;
+
+    void saveItem(ExternalData data) throws PathNotFoundException;
+
+    List<String> search(String basePath, String type, Map<String, String> constraints, String orderBy, int limit);
+
+}
